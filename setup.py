1d49b83e0e22daac9937a87600d9d76bc09cf020
Adrian Gao <adrian.gao@outlook.com>
2020-06-05 22:10:58 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+requires = [
+    'pandas',
+    'sqlalchemy',
+    'psycopg2-binary'
+]
+
 setup(name='frds',
       version='0.1',
       description='Financial Research Data Services',
       author='Mingze Gao',
       author_email='adrian.gao@outlook.com',
-      packages=['frds'])
+      packages=['frds'],
+      install_requires=requires)
