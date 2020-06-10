61ce2a0da2f72b733e73d8aa0cb1a9d274fd79cb
Mingze Gao <adrian.gao@outlook.com>
2020-06-10 12:08:06 +1000

    Update test_wrds.py

--- tests/test_wrds.py
+++ tests/test_wrds.py
@@ -14,13 +14,13 @@ class Login(unittest.TestCase):
 class GetData(unittest.TestCase):
 
     def test_get_data(self):
-        with DataManager() as dm:
+        with DataManager(obs=100) as dm:
             # Should be able to create np.recarry from shared memory
-            shm, shape, dtype = dm.get_dataset(sample_measure.dataset)
+            shm, shape, dtype = dm.get_dataset(sample_measure.datasets[0])
             nparray = np.recarray(shape=shape, dtype=dtype, buf=shm.buf)
             self.assertIsInstance(nparray, np.recarray)
             self.assertGreater(np.nanmean(nparray.at), 0)
             # The same dataset should have the same shared memory name
-            shm2, _, _ = dm.get_dataset(sample_measure.dataset)
+            shm2, _, _ = dm.get_dataset(sample_measure.datasets[0])
             self.assertEqual(shm.name, shm2.name)
             self.assertEqual(len(dm._datasets), 1)
