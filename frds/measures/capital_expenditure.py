0974993d292fbd99cc3fa1d530a5684c2e625505
Mingze Gao <adrian.gao@outlook.com>
2020-06-12 23:47:36 +1000

    Add measures.capital_expenditure

---
+++ frds/measures/capital_expenditure.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from numpy.lib import recfunctions as rfn
+from typing import List
+from ..data import Dataset
+
+name = 'CapitalExpenditure'
+description = 'Capital expenditures scaled by total assets.'
+datasets = [
+    Dataset(source='wrds', library='comp',
+            table='funda',
+            vars=['datadate', 'gvkey', 'at', 'capx', 'indfmt',
+                  'datafmt', 'popsrc', 'consol'],
+            date_vars=['datadate'])
+]
+variable_labels = {
+    name: 'Capital Expenditures scaled by Assets (Total)'
+}
+
+
+def estimate(nparrays: List[np.recarray]):
+
+    def filter_funda(x): return x[
+        np.in1d(x.datafmt, ('STD')) &
+        np.in1d(x.indfmt, ('INDL')) &
+        np.in1d(x.popsrc, ('D')) &
+        np.in1d(x.consol, ('C'))
+    ]
+
+    nparray = filter_funda(nparrays[0])
+    capx = np.true_divide(nparray.capx, nparray.at, where=(nparray.at != 0))
+    capx[np.isnan(nparray.at)] = np.nan
+    nparray = rfn.rec_append_fields(nparray, name, capx)
+    nparray.sort(order=['gvkey', 'datadate'])
+    return pd.DataFrame.from_records(nparray), variable_labels
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PostInstallCommand(install):
 
 
 setup(name='frds',
-      version='0.1.5',
+      version='0.1.6',
       description='Financial Research Data Services',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
