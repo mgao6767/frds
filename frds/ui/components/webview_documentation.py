f87b56078828349b14cca3f1e4a241bb2d7ca347
Adrian Gao <mingze.gao@sydney.edu.au>
2021-05-06 19:49:32 +1000

    Open external links in default system browser

--- frds/ui/components/webview_documentation.py
+++ frds/ui/components/webview_documentation.py
@@ -1,13 +1,22 @@
 from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
-from frds.settings import FRDS_MEASURES_PAGE
+from frds.settings import FRDS_HOME_PAGE, FRDS_MEASURES_PAGE
+
+class WebEnginePage(QWebEnginePage):
+
+    def acceptNavigationRequest(self, url,  _type, isMainFrame):
+        if _type == QWebEnginePage.NavigationTypeLinkClicked:
+            if FRDS_HOME_PAGE not in url.url():
+                QDesktopServices.openUrl(url)
+                return False
+        return True
 
 class Documentation(QWebEngineView):
 
-
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.home = QWebEnginePage()
+        self.home = WebEnginePage()
         self.home.setUrl(QUrl(FRDS_MEASURES_PAGE))
 
     def displayDoc(self):
