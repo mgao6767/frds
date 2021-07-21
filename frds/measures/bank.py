c3cefdedf7209a15d4fff1e59f218e4e7a0ae3e2
Adrian Gao <mingze.gao@sydney.edu.au>
2021-07-21 15:32:23 +1000

    Add marginal_expected_shortfall

--- frds/measures/bank.py
+++ frds/measures/bank.py
@@ -37,3 +37,39 @@ def absorption_ratio(
         )
     )
     return sum(eig_sorted[len(eig_sorted) - num_eigenvalues :]) / np.trace(cov)
+
+
+def marginal_expected_shortfall(
+    firm_returns: np.ndarray, market_returns: np.ndarray, q: float = 0.05
+) -> float:
+    """Marginal Expected Shortfall (MES).
+
+    Acharya, Pedersen, Philippon, and Richardson (2010) argue that each financial institutions contribution to \
+    systemic risk can be measured as its systemic expected shortfall (SES), i.e., its propensity to be undercapitalized \
+    when the system as a whole is undercapitalized. SES is a theoretical construct and the authors use the following 3 measures to proxy it:
+
+    1. The outcome of stress tests performed by regulators. The SES metric of a firm here is defined as the recommended capital that \
+        it was required to raise as a result of the stress test in February 2009.
+    2. The decline in equity valuations of large financial firms during the crisis, as measured by their cumulative equity return \
+        from July 2007 to December 2008.
+    3. The widening of the credit default swap spreads of large financial firms as measured by their cumulative CDS spread increases \
+        from July 2007 to December 2008.
+
+    Given these proxies, the authors seek to develop leading indicators which "predict" an institutions SES; \
+    these leading indicators are marginal expected shortfall (MES) and leverage (LVG).
+
+    The description above is from Bisias, Lo, and Valavanis.
+
+    Args:
+        firm_returns (np.ndarray): The time series of returns (equity or CDS) for the firm.
+        market_returns (np.ndarray): The time series of returns (equity or CDS) for the market as a whole. \
+            It is necessary to have enough data points so that there exists a value.
+        q (float, optional): The percentile. Range is [0, 1]. Deaults to 0.05.
+
+    Returns:
+        float: MES
+    """
+    assert firm_returns.shape == market_returns.shape
+    low_threshold = np.percentile(market_returns, q * 100)
+    worst_days = np.argwhere(market_returns < low_threshold)
+    return np.mean(firm_returns[worst_days])
--- tests/measures/test_bank.py
+++ tests/measures/test_bank.py
@@ -42,3 +42,61 @@ class AbsorptionRatioCase(unittest.TestCase):
         self.assertAlmostEqual(bank.absorption_ratio(self.more_data, 0.8), 0.9103, 4)
         self.assertAlmostEqual(bank.absorption_ratio(self.more_data, 0.9), 0.9740, 4)
         self.assertAlmostEqual(bank.absorption_ratio(self.more_data, 1), 1, 4)
+
+
+class MarginalExpectedShortfallCase(unittest.TestCase):
+    def setUp(self):
+        # The data in the doc by Dimitrios Bisias, Andrew W. Lo, and Stavros Valavanis
+        self.firm_returns = np.array(
+            [
+                -0.1595,
+                0.1211,
+                -0.0806,
+                -0.0291,
+                0.0897,
+                -0.0254,
+                0.1210,
+                0.0132,
+                -0.1214,
+                0.1901,
+                0.0243,
+            ]
+        )
+        self.market_returns = np.array(
+            [
+                -0.0205,
+                -0.0510,
+                0.0438,
+                0.0914,
+                0.0243,
+                -0.1051,
+                0.0121,
+                0.0221,
+                -0.0401,
+                -0.0111,
+                -0.0253,
+            ]
+        )
+        # simulated data
+        np.random.seed(0)
+        self.sim_firm_returns = np.round(np.random.normal(0, 1, (100,)) / 100, 3)
+        self.sim_market_returns = np.round(np.random.normal(0, 1, (100,)) / 100, 3)
+
+    def test_mes(self):
+        # Results computed using the Matlab code by Dimitrios Bisias, Andrew W. Lo, and Stavros Valavanis
+        mes = bank.marginal_expected_shortfall(
+            self.firm_returns, self.market_returns, q=0.05
+        )
+        self.assertAlmostEqual(mes, -0.0254, 4)
+
+    def test_simulated_data(self):
+        # Results computed using the Matlab code by Dimitrios Bisias, Andrew W. Lo, and Stavros Valavanis
+        mes = bank.marginal_expected_shortfall(
+            self.sim_firm_returns, self.sim_market_returns, q=0.01
+        )
+        self.assertAlmostEqual(mes, -0.015, 4)
+
+        mes = bank.marginal_expected_shortfall(
+            self.sim_firm_returns, self.sim_market_returns, q=0.05
+        )
+        self.assertAlmostEqual(mes, 0.0016, 4)
