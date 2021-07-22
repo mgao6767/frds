1adbf7d8a6dbf46f11c29df2e81fd7f6b27d71f8
Adrian Gao <mingze.gao@sydney.edu.au>
2021-07-22 19:22:13 +1000

    Trying to fix "module has no attribute" error when building docs
    frds is imported during setup, so probably not a good idea to import frds.measures, which ultimately imports the c++ extension that hasn't been compiled yet.

--- frds/__init__.py
+++ frds/__init__.py
@@ -1,11 +1,13 @@
 import sys
 
-__version__ = "0.7.2"
-__description__ = "Financial Research Data Services"
-__author__ = "Mingze Gao"
-__author_email__ = "adrian.gao@outlook.com"
-__project_url__ = "https://frds.io"
-__github_url__ = "https://github.com/mgao6767/frds/"
+meta = dict(
+    __version__="0.7.2",
+    __description__="Financial Research Data Services",
+    __author__="Mingze Gao",
+    __author_email__="adrian.gao@outlook.com",
+    __project_url__="https://frds.io",
+    __github_url__="https://github.com/mgao6767/frds/",
+)
 
 if sys.version_info.major < 3 and sys.version_info.minor < 8:
     print("Python3.8 and higher is required.")
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     print("Numpy needs to be installed.")
     sys.exit(1)
 
-import frds
+from frds import meta
 
 if sys.platform == "linux" or sys.platform == "linux2":
     # linux
@@ -39,13 +39,13 @@ mod_isolation_forest = Extension(
 
 setup(
     name="frds",
-    version=frds.__version__,
-    description=frds.__description__,
+    version=meta.__version__,
+    description=meta.__description__,
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author=frds.__author__,
-    author_email=frds.__author_email__,
-    url=frds.__github_url__,
+    author=meta.__author__,
+    author_email=meta.__author_email__,
+    url=meta.__github_url__,
     packages=find_namespace_packages(),
     package_data={
         "": ["LICENSE", "README.md", "*.cpp", "*.hpp"],
