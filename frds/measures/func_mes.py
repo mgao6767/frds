207dcbef9353a644fb1e0465a7e9cd085aaf2e35
Adrian Gao <mingze.gao@sydney.edu.au>
2021-07-26 15:46:35 +1000

    Improve the doc for MES

--- frds/measures/func_mes.py
+++ frds/measures/func_mes.py
@@ -6,32 +6,38 @@ def marginal_expected_shortfall(
 ) -> float:
     """Marginal Expected Shortfall (MES).
 
-    Acharya, Pedersen, Philippon, and Richardson (2010) argue that each financial institutions contribution to \
-    systemic risk can be measured as its systemic expected shortfall (SES), i.e., its propensity to be undercapitalized \
-    when the system as a whole is undercapitalized. SES is a theoretical construct and the authors use the following 3 measures to proxy it:
+    The firm's average return during the 5% worst days for the market.
 
-    1. The outcome of stress tests performed by regulators. The SES metric of a firm here is defined as the recommended capital that \
-        it was required to raise as a result of the stress test in February 2009.
-    2. The decline in equity valuations of large financial firms during the crisis, as measured by their cumulative equity return \
-        from July 2007 to December 2008.
-    3. The widening of the credit default swap spreads of large financial firms as measured by their cumulative CDS spread increases \
-        from July 2007 to December 2008.
-
-    Given these proxies, the authors seek to develop leading indicators which "predict" an institutions SES; \
-    these leading indicators are marginal expected shortfall (MES) and leverage (LVG).
-
-    The description above is from Bisias, Lo, and Valavanis.
+    MES measures how exposed a firm is to aggregate tail shocks and, interestingly, together with leverage, \
+    it has a significant explanatory power for which firms contribute to a potential crisis as noted by \
+    [Acharya, Pedersen, Philippon, and Richardson (2010)](https://doi.org/10.1093/rfs/hhw088).
+    
+    It is used to construct the [Systemic Expected Shortfall](/measures/systemic_expected_shortfall/).
 
     Args:
-        firm_returns (np.ndarray): The time series of returns (equity or CDS) for the firm.
-        market_returns (np.ndarray): The time series of returns (equity or CDS) for the market as a whole. \
-            It is necessary to have enough data points so that there exists a value.
+        firm_returns (np.ndarray): (n_days,) array of the returns (equity or CDS) for the firm.
+        market_returns (np.ndarray): (n_days,) array of the returns (equity or CDS) for the market as a whole.
         q (float, optional): The percentile. Range is [0, 1]. Deaults to 0.05.
 
     Returns:
         float: The marginal expected shortfall of firm $i$ at time $t$.
 
+    Examples:
+        >>> from numpy.random import RandomState
+        >>> from frds.measures import marginal_expected_shortfall
+
+        Let's simulate some returns for the firm and the market
+        >>> rng = RandomState(0)
+        >>> firm_returns = rng.normal(0,1,100)
+        >>> mkt_returns = rng.normal(0,1,100)
+
+        Compute the MES.
+        >>> marginal_expected_shortfall(firm_returns, mkt_returns)
+        0.13494025343324562
+
     References:
+        * [Acharya, Pedersen, Philippon, and Richardson (2017)](https://doi.org/10.1093/rfs/hhw088),
+            Measuring systemic risk, *The Review of Financial Studies*, 30, (1), 2-47.
         * [Bisias, Flood, Lo, and Valavanis (2012)](https://doi.org/10.1146/annurev-financial-110311-101754),
             A survey of systemic risk analytics, *Annual Review of Financial Economics*, 4, 255-296.
 
@@ -42,6 +48,7 @@ def marginal_expected_shortfall(
         * [Distress Insurance Premium](/measures/distress_insurance_premium/)
         * [Systemic Expected Shortfall (SES)](/measures/systemic_expected_shortfall/)
     """
+    assert 0 <= q <= 1
     assert firm_returns.shape == market_returns.shape
     low_threshold = np.percentile(market_returns, q * 100)
     worst_days = np.argwhere(market_returns < low_threshold)
